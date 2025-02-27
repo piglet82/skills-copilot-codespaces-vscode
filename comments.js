cc74c5f3f88dcdd7e6bc65a2191aea91e0cf3fbf
piglet82 <piglet82@o2.pl>
2025-02-27 19:40:45 +0000

    modified:   comments.js

--- comments.js
+++ comments.js
@@ -7,8 +7,28 @@ var app = express();
 app.get('/comments', function (req, res) {
   res.json([
     { message: 'hello', author: 'John' },
-    { message: 'hi', author: 'Jane' }
+    { message: 'hi', author: 'Jane' },
+    { message: 'hi2', author: 'Jane2' }
   ]);
 });
 
-app.listen(3000);
+app.listen(3000, function() {
+  console.log('Server is running on port 3000');
+});
+
+var request = require('supertest');
+request(app)
+  .get('/comments')
+  .expect('Content-Type', /json/)
+  .expect(200)
+  .expect(function(res) {
+    if (!res.body.some(commentx => commentx.message === 'hello' && commentx.author === 'John')) {
+      throw new Error('Expected comment not found');
+    }
+  })
+  .end(function(err, res) {
+    if (err) {
+      throw err;
+    }
+    console.log(res.body);
+  });
